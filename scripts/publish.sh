726d8b53c3930e970b386c7e4133f38b73cdf558
Mathias Toftås <github.simplify147@passmail.net>
2024-12-10 17:36:29 +0100

    added publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd "$SCRIPT_DIR/.."
+
+source ./venv/bin/activate
+
+python -m build
+python -m twine upload dist/*
