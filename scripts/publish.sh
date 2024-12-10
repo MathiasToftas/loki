4170bb58f7ba52c9a6d2dd9a13f5129fa7cf446c
Mathias Toftås <github.simplify147@passmail.net>
2024-12-10 17:43:36 +0100

    improved error msg in publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
+read -p "Have you updated the version number in pyproject.toml? (yes/no): " version_updated
+
+if [[ "$version_updated" != "yes" ]]; then
+    echo "Please update the version number in pyproject.toml before running this script."
+    exit 1
+fi
+
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd "$SCRIPT_DIR/.."
 
 source ./venv/bin/activate
-
+rm  dist/*
 python -m build
 python -m twine upload dist/*
